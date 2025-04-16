6f164da72b7a29ee7e056e6be90046bffaa03973
michael <michael@mabbott.dev>
2025-04-17 00:49:03 +0100

    chore: misc

--- apis/management-api/internal/accounts/router.go
+++ apis/management-api/internal/accounts/router.go
@@ -11,14 +11,30 @@ type router struct {
 	api huma.API
 }
 
+type AccountResponse struct {
+	Body struct {
+		Message string `json:"message" example:"test" descripton:"another test"`
+	} `json:"body"`
+}
+
 type AccountsResponse struct {
 	Body struct {
 		Message string `json:"message" example:"test" descripton:"another test"`
 	} `json:"body"`
 }
 
+type GetAccountInput struct {
+	ID string `path:"id" doc:"ID of account"`
+}
+
 func (r *router) Accounts(ctx context.Context, input *struct{}) (*AccountsResponse, error) {
 	resp := &AccountsResponse{}
+	resp.Body.Message = "Hello, world!"
+	return resp, nil
+}
+
+func (r *router) Account(ctx context.Context, input GetAccountInput) (*AccountResponse, error) {
+	resp := &AccountResponse{}
 	resp.Body.Message = "Hello, world!"
 	return resp, nil
 }
@@ -30,6 +46,15 @@ func (r *router) Router() {
 		Path:        "/accounts",
 		Summary:     "Get accounts",
 		Description: "Get accounts.",
+		Tags:        []string{"Accounts"},
+	}, r.Accounts)
+
+	huma.Register(r.api, huma.Operation{
+		OperationID: "get-account",
+		Method:      http.MethodGet,
+		Path:        "/accounts/{id}",
+		Summary:     "Get account",
+		Description: "Get account by id.",
 		Tags:        []string{"Accounts"},
 	}, r.Accounts)
 }
